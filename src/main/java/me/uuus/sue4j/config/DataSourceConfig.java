8ee9200e1fe739913c63e19ad76e181045c3e76e
Mr.Su <swb0917@gmail.com>
2016-09-23 22:55:34 +0800

    update  something...

--- src/main/java/me/uuus/sue4j/config/DataSourceConfig.java
+++ src/main/java/me/uuus/sue4j/config/DataSourceConfig.java
@@ -2,42 +2,27 @@ package me.uuus.sue4j.config;
 
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.jdbc.DatabaseDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 /**
+ * 配置数据库连接池
+ *
  * @author Mr.Su[swb0917@gmail.com]
  */
 
 
 @Configuration
-@EnableConfigurationProperties(DataSourceProperties.class)
 public class DataSourceConfig {
 
     private static final String DRUID_PREFIX = "spring.datasource.druid";
 
-    @Bean
+    @Bean(destroyMethod = "close")
     @ConfigurationProperties(DataSourceConfig.DRUID_PREFIX)
-    public DataSource dataSource(
-            DataSourceProperties properties) {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setDriverClassName(properties.determineDriverClassName());
-        dataSource.setUrl(properties.determineUrl());
-        dataSource.setUsername(properties.determineUsername());
-        dataSource.setPassword(properties.determinePassword());
-        DatabaseDriver databaseDriver = DatabaseDriver
-                .fromJdbcUrl(properties.determineUrl());
-        String validationQuery = databaseDriver.getValidationQuery();
-        if (validationQuery != null) {
-            dataSource.setTestOnBorrow(true);
-            dataSource.setValidationQuery(validationQuery);
-        }
-        return dataSource;
+    public DataSource dataSource() {
+        return new DruidDataSource();
     }
 }
